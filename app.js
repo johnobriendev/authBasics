faf103b25076ad125c77d8bf40deea9eaf812e05
John O'Brien <johnobrien.dev@gmail.com>
2024-05-29 17:21:03 -0400

    add validation/sanitation to log in

--- app.js
+++ app.js
@@ -131,11 +131,36 @@ async (req, res, next) => {
 
 app.post(
   "/log-in",
+  [
+    // Validation rules
+    body('username')
+      .trim()
+      .isLength({ min: 1 }).withMessage('Username is required.')
+      .escape(),
+    body('password')
+      .trim()
+      .isLength({ min: 1 }).withMessage('Password is required.')
+      .escape()
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // There are validation errors. Render the form again with sanitized values/error messages.
+      return res.render('index', { 
+        errors: errors.array(),
+        user: req.user
+      });
+    }
+  
   passport.authenticate("local", {
       successRedirect: "/",
       failureRedirect: "/"
-  })
+  })(req, res, next);
+
+}
 );
+
+
 
 app.get("/log-out", (req, res, next) => {
   req.logout((err) => {
